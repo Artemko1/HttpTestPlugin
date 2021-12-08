3bb7225d3907bb304e0068c8b84d3877148e87b4
life-exe <lifeexe.art@gmail.com>
2021-12-08 22:22:18 +0200

    review 1 step

--- Plugins/HttpTestPlugin/Source/HttpTestPlugin/Public/HTTPDataTypes.h
+++ Plugins/HttpTestPlugin/Source/HttpTestPlugin/Public/HTTPDataTypes.h
@@ -6,7 +6,7 @@ USTRUCT(BlueprintType)
 struct FInfoData
 {
 	GENERATED_BODY();
-	
+
 	int32 RecievedId;
 	FString ReceivedName;
 	FString ReceivedStatus;
@@ -15,12 +15,14 @@ struct FInfoData
 	FString ReceivedGender;
 	FString ReceivedURL;
 	FString ReceivedCreated;
+	FString ImageURL;
 	UPROPERTY()
 	UTexture2DDynamic* Texture;
 
 	FInfoData() {}
 	FInfoData(int32 NewRecievedId, const FString& NewReceivedName, const FString& NewReceivedStatus, const FString& NewReceivedSpecies,
-		const FString NewReceivedType, const FString& NewReceivedGender, const FString& NewReceivedURL, const FString& NewReceivedCreated, UTexture2DDynamic* NewTexture)
+		const FString NewReceivedType, const FString& NewReceivedGender, const FString& NewReceivedURL, const FString& NewReceivedCreated,
+		UTexture2DDynamic* NewTexture)
 	{
 		this->RecievedId = NewRecievedId;
 		this->ReceivedName = NewReceivedName;
@@ -33,11 +35,8 @@ struct FInfoData
 		this->Texture = NewTexture;
 	}
 
-	FInfoData(const FInfoData& Other)
-	{
-		*this = Other;
-	}
-	
+	FInfoData(const FInfoData& Other) { *this = Other; }
+
 	void operator=(const FInfoData& Other)
 	{
 		this->RecievedId = Other.RecievedId;
@@ -50,4 +49,31 @@ struct FInfoData
 		this->ReceivedCreated = Other.ReceivedCreated;
 		this->Texture = Other.Texture;
 	}
+
+	void Reset()
+	{
+		RecievedId = 0;
+		ReceivedName = "";
+		ReceivedStatus = "";
+		ReceivedSpecies = "";
+		ReceivedType = "";
+		ReceivedGender = "";
+		ReceivedURL = "";
+		ReceivedCreated = "";
+        ImageURL = "";
+		Texture = nullptr;
+	}
+
+	void Parse(TSharedPtr<FJsonObject> JsonObject) 
+    {
+        RecievedId = JsonObject->GetIntegerField("id");
+		ReceivedName = JsonObject->GetStringField("name");
+		ReceivedStatus = JsonObject->GetStringField("status");
+		ReceivedSpecies = JsonObject->GetStringField("species");
+		ReceivedType = JsonObject->GetStringField("type");
+		ReceivedGender = JsonObject->GetStringField("gender");
+		ReceivedURL = JsonObject->GetStringField("url");
+		ReceivedCreated = JsonObject->GetStringField("created");
+		ImageURL = JsonObject->GetStringField("image");
+    }
 };
