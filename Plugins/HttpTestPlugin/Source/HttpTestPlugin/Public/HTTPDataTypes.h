2ae63ff00e634e1f3fc304dad13a6ad0c145bb0a
life-exe <lifeexe.art@gmail.com>
2021-12-09 10:47:57 +0200

    review step 5

--- Plugins/HttpTestPlugin/Source/HttpTestPlugin/Public/HTTPDataTypes.h
+++ Plugins/HttpTestPlugin/Source/HttpTestPlugin/Public/HTTPDataTypes.h
@@ -15,7 +15,7 @@ struct FInfoData
 	FString Gender;
 	FString URL;
 	FString Created;
-	FString ImageURL;
+	FString Image;
 	UPROPERTY()
 	UTexture2DDynamic* Texture;
 
@@ -29,20 +29,22 @@ struct FInfoData
 		Gender.Empty();
 		URL.Empty();
 		Created.Empty();
-		ImageURL.Empty();
+		Image.Empty();
 		Texture = nullptr;
 	}
 
+#define TO_JSON_ID(param) FString(#param).ToLower()
+
 	void Parse(TSharedPtr<FJsonObject> JsonObject)
 	{
-		Id = JsonObject->GetIntegerField("id");
-		Name = JsonObject->GetStringField("name");
-		Status = JsonObject->GetStringField("status");
-		Species = JsonObject->GetStringField("species");
-		Type = JsonObject->GetStringField("type");
-		Gender = JsonObject->GetStringField("gender");
-		URL = JsonObject->GetStringField("url");
-		Created = JsonObject->GetStringField("created");
-		ImageURL = JsonObject->GetStringField("image");
+		Id = JsonObject->GetIntegerField(TO_JSON_ID(Id));
+		Name = JsonObject->GetStringField(TO_JSON_ID(Name));
+		Status = JsonObject->GetStringField(TO_JSON_ID(Status));
+		Species = JsonObject->GetStringField(TO_JSON_ID(Species));
+		Type = JsonObject->GetStringField(TO_JSON_ID(Type));
+		Gender = JsonObject->GetStringField(TO_JSON_ID(Gender));
+		URL = JsonObject->GetStringField(TO_JSON_ID(URL));
+		Created = JsonObject->GetStringField(TO_JSON_ID(Created));
+		Image = JsonObject->GetStringField(TO_JSON_ID(Image));
 	}
 };
